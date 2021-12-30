01a2e4a65073967e9f698ab2463a4822226c8c5b
MuhamadAkmalAmran <akmal.amran52@gmail,com>
2021-12-31 00:35:49 +0700

    updatetugasakmal

--- src/main/java/com/ti/tugas/javaA/Akmal/Controllertugas.java
+++ src/main/java/com/ti/tugas/javaA/Akmal/Controllertugas.java
@@ -17,38 +17,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Controllertugas {
     
+    
+    
     @RequestMapping("/viewbayar")
     public String inputanuser(HttpServletRequest data, Model buah){
+        
+        prosesClass pc = new prosesClass();
+        
         String nbuah = data.getParameter("var_namabuah");
         String hbuah = data.getParameter("var_hargakilo");
-        Integer chbuah = Integer.valueOf(hbuah);
         String jbuah = data.getParameter("var_jumlahbeli");
-        Double cjbuah = Double.valueOf(jbuah);
-        Double jumlahbayar = chbuah * cjbuah;
-        Double totalbayar = null;
-        Integer diskon = 0;
-        Double hargadiskon = 0.0;
-
-        if(jumlahbayar < 16000){
-            totalbayar = jumlahbayar - (jumlahbayar*0/100);
-            hargadiskon = jumlahbayar*diskon/100;
-            
-        }else if( jumlahbayar < 25000){
-            diskon = 10;
-            totalbayar = jumlahbayar - (jumlahbayar*10/100);
-            hargadiskon = jumlahbayar*diskon/100;
-        }else {
-            diskon = 15;
-            totalbayar = jumlahbayar - (jumlahbayar*diskon/100);
-            hargadiskon = jumlahbayar*diskon/100;
-        }
+        
+        Double hargabuah = pc.getharga(jbuah);
+        Double jumlahbuah = pc.getjumlah(hbuah);
+        Double jumlahbayar = pc.getjumlahbayar(hargabuah, jumlahbuah);
+        String disc = pc.getdisc(jumlahbayar);
+        Double hargadiskon = pc.gethargadisc(jumlahbayar, Integer.valueOf(disc));
+        Double totalbayar =  pc.gettotalbayar(jumlahbayar, hargadiskon);
+        pc.gettdiskon(totalbayar, jumlahbayar, hargabuah, Integer.valueOf(disc));
+        
+        
         
         buah.addAttribute("name",nbuah);
-        buah.addAttribute("price",chbuah);
-        buah.addAttribute("kilo",cjbuah);
+        buah.addAttribute("price",hbuah);
+        buah.addAttribute("kilo",jbuah);
         buah.addAttribute("tbayar",totalbayar);
-        buah.addAttribute("discountrp",hargadiskon);
-        buah.addAttribute("disc",diskon);
+        buah.addAttribute("hargadisc",hargadiskon);
+        buah.addAttribute("disc",disc);
         buah.addAttribute("total10",jumlahbayar);
         return "Akmal";
     }
---
+++ src/main/java/com/ti/tugas/javaA/Akmal/prosesClass.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ti.tugas.javaA.Akmal;
+
+/**
+ *
+ * @author USER_64
+ */
+public class prosesClass {
+    
+    public Double getharga(String hbuah){
+       
+        Double hargabuah = Double.valueOf(hbuah);
+        
+        return hargabuah;
+    }
+    
+    public Double getjumlah(String jbuah){
+    
+        Double jumlahbuah = Double.valueOf(jbuah);
+    
+        return jumlahbuah;
+    }
+    
+    public Double gettotalbayar(Double jbayar, Double hdiskon){
+    
+        Double tbayar = jbayar - hdiskon;
+        
+        return tbayar;
+    }
+    
+    public Double getjumlahbayar(Double hbuah, Double jbuah){
+    
+        Double jbayar = hbuah * jbuah;
+    
+        return jbayar;
+    }
+    
+    public String getdisc(Double jbayar){
+    
+        String disc = null;
+        
+        if (jbayar <16000) {
+            disc = "0";
+        }else if (jbayar <25000){
+            disc = "10";
+        }else{
+            disc = "15";
+        }
+        return disc;
+    }
+    
+    public Double gethargadisc(Double jbayar, Integer disk){
+        
+        Double hdiskon = jbayar*disk/100;
+    
+        return hdiskon;
+    }
+    
+    public void gettdiskon(Double tbayar, Double jumbayar, Double hdiskon, Integer diskon){
+        
+        if(jumbayar < 16000){
+            diskon = 0;
+            tbayar = jumbayar - (jumbayar*diskon/100);
+            hdiskon = jumbayar*diskon/100;
+            
+        }else if( jumbayar < 25000){
+            diskon = 10;
+            tbayar = jumbayar - (jumbayar*diskon/100);
+            hdiskon = jumbayar*diskon/100;
+        }else {
+            diskon = 15;
+            tbayar = jumbayar - (jumbayar*diskon/100);
+            hdiskon = jumbayar*diskon/100;
+        }
+    }
+}
